775da6d2d75cd4d4f2a5bd9b5ab07cc616e8f1d5
Jesus1103LG <rieteocaeras@gmail.com>
2024-12-17 07:08:19 -0400

    add landing in index

--- index.php
+++ index.php
@@ -1,10 +1,79 @@
 <?php $subTitulo = "Landing"; ?>
 <?php include("src/layout/header.php"); ?>
 
-
-
-
-
+<main>
+    <header class="header-main">
+        <img src="src/public/img1.jpg" alt="Imagen principal" />
+        <div class="div-lomejor">
+            <p>LO MEJOR PARA TI</p>
+        </div>
+        <div class="header-main-parrafos">
+            <h1>Bienvenidos a PeliShop</h1>
+            <p>
+                En PeliShop, nos enfocamos en ofrecer una amplia variedad de zapatos
+                de las mejores marcas a precios competitivos. Nuestra misión es
+                brindar a nuestros clientes una experiencia de compra fácil y
+                segura.
+            </p>
+        </div>
+    </header>
+    <section class="first-section">
+        <div class="first-section-div">
+            <p>
+                Toda la comodidad para tus pies, en todas los tallas y colores, en
+                PeliShop te ofresemos <b>la mejor calidad</b> a
+                <b>los mejores precios</b>, y con
+                <b>las mejores colaboraciones</b> con Gianpiero, Tesla, Microsoft,
+                Ardidas, BadBull y PDVSA.
+            </p>
+        </div>
+        <img class="first-section-img" src="src/public/img3.png" alt="zapatosss" />
+        <img class="first-section-imgmain" src="src/public/img2.jpg" alt="zapata" />
+    </section>
+    <div class="mid-section">
+        <p>Escoge tu ENM</p>
+    </div>
+    <img class="mid-img" src="src/public/img4.jpg" alt="" />
+    <div class="div-insta">
+        <h2 class="h2-insta">Insta Section</h2>
+        <div class="div-insta-post"></div>
+        <button class="left"><--< /button>
+                <button class="right">--></button>
+    </div>
+</main>
+<footer>
+    <div class="footer">
+        <img class="footer-logo" src="src/public/logo.png" alt="Logo" />
+        <div>
+            <h3>Productos</h3>
+            <p>Zapatos</p>
+            <p>Calzado</p>
+            <p>Zapato Calzado</p>
+        </div>
+        <div>
+            <h3>Colaboradores</h3>
+            <p>Tesla</p>
+            <p>PDVSA</p>
+            <p>Microsoft</p>
+        </div>
+        <div>
+            <h3>Redes</h3>
+            <a href="https://www.instagram.com/jsuslopez91/" target="_blank">Instagram</a>
+            <a
+                href="https://www.facebook.com/profile.php?id=100006749236651"
+                target="_blank">Facebook</a>
+            <a href="https://www.tiktok.com/@jsuslopez91" target="_blank">TikTok</a>
+        </div>
+        <div>
+            <h3>PeliShop</h3>
+            <a href="">Kevin</a>
+            <a href="">Luis</a>
+            <a href="">Gabriel</a>
+        </div>
+    </div>
+    <p class="copy">Copyright © The Jackets Team ft. PeliRed</p>
+</footer>
+<script src="Javascript/inde.js"></script>
 </body>
 
 </html>
