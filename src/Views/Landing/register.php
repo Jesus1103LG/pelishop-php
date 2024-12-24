f7c5336bc801ff00d86067535630df6ee9c51fbe
Jesus1103LG <rieteocaeras@gmail.com>
2024-12-24 10:07:44 -0400

    edit: form sign up and style

--- src/Views/Landing/register.php
+++ src/Views/Landing/register.php
@@ -4,39 +4,7 @@
 <div class="content">
     <form id="form" method="POST" action="">
         <div class="form-div">
-            <label for="">Nombre</label>
-            <input id="nombre-signup" type="text" required />
-        </div>
-        <div class="form-div">
-            <label for="">Apellido</label>
-            <input id="apellido-signup" type="text" required />
-        </div>
-        <div class="form-div">
-            <label for="">Email</label>
-            <input id="email-signup" type="email" required />
-        </div>
-        <div class="form-div">
-            <label for="">Telefono</label>
-            <div id="form-div-tlf">
-                <input class="code" type="text" required readonly value="+58" />
-                <input
-                    id="telefono-signup"
-                    type="number"
-                    required
-                    max="9999999999"
-                    oninput="limitDigits(this)" />
-            </div>
-        </div>
-        <div class="form-div">
-            <label for="">Fecha de nacimiento</label>
-            <input id="fechaNc-signup" type="date" required />
-        </div>
-        <div class="form-div">
-            <label for="">Foto</label>
-            <input id="foto-signup" type="file" required />
-        </div>
-        <div class="form-div">
-            <label for="">¿Qué eres?</label>
+            <label for="rol">¿Qué eres?</label>
             <select name="rol" id="rol" required>
                 <option value="" disabled selected>Seleccione un opcion</option>
                 <option value="2">Cliente</option>
@@ -44,10 +12,53 @@
             </select>
         </div>
         <div class="form-div">
-            <label for="">Password</label>
-            <input id="password-signup" type="password" required minlength="8" />
+            <label for="tipo-doc cedula">Documento de identidad</label>
+            <div class="identidad">
+                <select name="tipo-doc" id="tipo-doc" required>
+                    <option value="" disabled selected>Seleccione un opcion</option>
+                    <option value="V">V</option>
+                    <option value="E">E</option>
+                    <option value="J">J</option>
+                    <option value="G">G</option>
+                </select>
+                <input name="cedula" id="cedula" type="text" required />
+            </div>
         </div>
-        <button id="btn-signup">SIGN UP</button>
+        <div class="form-div">
+            <label for="nombre">Nombre</label>
+            <input name="nombre" id="nombre" type="text" required />
+        </div>
+        <div class="form-div">
+            <label for="apellido">Apellido</label>
+            <input name="apellido" id="apellido" type="text" required />
+        </div>
+        <div class="form-div">
+            <label for="email">Email</label>
+            <input name="email" id="email" type="email" required />
+        </div>
+        <div class="form-div">
+            <label for="telefono">Telefono</label>
+            <div id="form-div-tlf">
+                <input class="code" type="text" required readonly value="+58" />
+                <input
+                    name="telefono"
+                    id="telefono"
+                    type="number"
+                    required
+                    max="9999999999"
+                    oninput="limitDigits(this)" />
+            </div>
+        </div>
+        <div class="form-div">
+            <label for="fechaNc">Fecha de nacimiento</label>
+            <input name="fechaNc" id="fechaNc" type="date" required />
+        </div>
+
+        <div class="form-div">
+            <label for="password">Password</label>
+            <input name="password" id="password" type="password" required minlength="8" />
+        </div>
+        <button id="btn">SIGN UP</button>
     </form>
     <p>Ya tienes una Cuenta? <a href="login.html">Login</a></p>
 </div>
