77daeca79330cf058206f58179016dea96381ea7
Jesus1103LG <rieteocaeras@gmail.com>
2025-01-10 09:09:48 -0400

    add: enhance authentication redirection logic for different user roles

--- src/helpers/auth.php
+++ src/helpers/auth.php
@@ -18,17 +18,31 @@ function requireAuth()
 function redirectIfAuthenticated()
 {
     session_start();
-    if (isset($_SESSION["valid"])) {
-        header("Location: /peliShop_PHP/Cliente/home");
-        exit();
+    if (isset($_SESSION["valid"]) && isset($_SESSION["rol"])) {
+        if ($_SESSION["rol"] == 1) {
+            header("Location: /peliShop_PHP/Admin/home");
+            exit();
+        } else if ($_SESSION["rol"] == 2) {
+            header("Location: /peliShop_PHP/Cliente/home");
+            exit();
+        } else if ($_SESSION["rol"] == 3) {
+            header("Location: /peliShop_PHP/Empresa/home");
+            exit();
+        }
     }
 }
 
 function redirectTo404()
 {
     session_start();
-    if (isset($_SESSION["valid"])) {
-        header("Location: /peliShop_PHP/Cliente/_404");
+    if (isset($_SESSION["valid"]) && isset($_SESSION["rol"])) {
+        if ($_SESSION["rol"] == 1) {
+            header("Location: /peliShop_PHP/Admin/_404");
+        } else if ($_SESSION["rol"] == 2) {
+            header("Location: /peliShop_PHP/Cliente/_404");
+        } else if ($_SESSION["rol"] == 3) {
+            header("Location: /peliShop_PHP/Empresa/_404");
+        }
     } else {
         header("Location: /peliShop_PHP/Landing/_404");
     }
