50f0acaebb3ea88d1c08512c767b7b102c756d16
Jesus1103LG <rieteocaeras@gmail.com>
2025-01-17 07:44:14 -0400

    add: create tablas view for Admin with navigation links to various sections

---
+++ src/Views/Admin/tablas.php
@@ -0,0 +1,59 @@
+<?php $subTitulo = "Tablas"; ?>
+<?php include("src/layout/header.php"); ?>
+
+<main class="main-admin">
+    <div class="container">
+        <a href="ventas" class="card">
+            <h5 class="card-title">Ventas</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="clientes" class="card">
+            <h5 class="card-title">Clientes</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="empresas" class="card">
+            <h5 class="card-title">Empresas</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="productos" class="card">
+            <h5 class="card-title">Productos</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="categorias" class="card">
+            <h5 class="card-title">Categorias</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="admins" class="card">
+            <h5 class="card-title">Admins</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="admins" class="card">
+            <h5 class="card-title">Direcciones</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="admins" class="card">
+            <h5 class="card-title">Estados</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="admins" class="card">
+            <h5 class="card-title">Ciudades</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="admins" class="card">
+            <h5 class="card-title">Venta_Producto</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="admins" class="card">
+            <h5 class="card-title">Categorias_Productos</h5>
+            <p class="card-description"></p>
+        </a>
+        <a href="admins" class="card">
+            <h5 class="card-title">Roles</h5>
+            <p class="card-description"></p>
+        </a>
+    </div>
+</main>
+
+</body>
+
+</html>
