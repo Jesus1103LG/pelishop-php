bd24bbee2f616c3520c684ef1c6041357f735709
Jesus1103LG <rieteocaeras@gmail.com>
2025-01-23 08:23:36 -0400

    fix: add validation for numeric input with up to two decimal places in limitDigits function

--- src/public/javascript/signup.js
+++ src/public/javascript/signup.js
@@ -2,4 +2,9 @@ function limitDigits(input) {
   if (input.value.length > 10) {
     input.value = input.value.slice(0, 10);
   }
+  // Validar que solo se permitan números y un solo punto decimal
+  const regex = /^\d*\.?\d{0,2}$/; // Permitir hasta 2 decimales
+  if (!regex.test(input.value)) {
+    input.value = input.value.slice(0, -1);
+  }
 }
