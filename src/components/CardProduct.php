ce31e93f3c9f10b935376ba029d4504708f6c92d
Jesus1103LG <rieteocaeras@gmail.com>
2025-01-24 16:03:44 -0400

    add: create CardProduct component for product display

---
+++ src/components/CardProduct.php
@@ -0,0 +1,23 @@
+<div class="product-card">
+    <a href="#">
+        <img class="product-image" src="/peliShop_PHP/src/public/img1.jpg" alt="product image" />
+    </a>
+    <div class="product-details">
+        <a href="#">
+            <h5 class="product-title">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+        </a>
+        <div class="product-rating">
+            <div class="stars">
+                <svg class="star-icon yellow" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+                <!-- Repeated SVG for stars -->
+            </div>
+            <span class="rating-label">5.0</span>
+        </div>
+        <div class="product-pricing">
+            <span class="price">$599</span>
+            <a href="#" class="add-to-cart-button">Add to cart</a>
+        </div>
+    </div>
+</div>
