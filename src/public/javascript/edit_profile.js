dea1462d38b5b2d0765ebf5699a0d78659469c5a
Jesus1103LG <rieteocaeras@gmail.com>
2025-01-28 06:49:36 -0400

    feat: add function to load cities for Empresa profile editing

--- src/public/javascript/edit_profile.js
+++ src/public/javascript/edit_profile.js
@@ -13,7 +13,6 @@ function cargarCiudadesAdmin(estadoId) {
       return response.text(); // Procesamos la respuesta como texto
     })
     .then((html) => {
-      console.log(html);
       // Insertamos el HTML en el select de ciudades
       const ciudadSelect = document.getElementById("ciudad");
       ciudadSelect.innerHTML = html;
@@ -25,6 +24,30 @@ function cargarCiudadesAdmin(estadoId) {
 
 function cargarCiudades(estadoId) {
   fetch("http://localhost/peliShop_PHP/Cliente/edit_profile", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ estado_id: estadoId }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error en la solicitud: " + response.status);
+      }
+      return response.text(); // Procesamos la respuesta como texto
+    })
+    .then((html) => {
+      // Insertamos el HTML en el select de ciudades
+      const ciudadSelect = document.getElementById("ciudad");
+      ciudadSelect.innerHTML = html;
+    })
+    .catch((error) => {
+      console.error("Error al cargar las ciudades:", error);
+    });
+}
+
+function cargarCiudadesEmpresa(estadoId) {
+  fetch("http://localhost/peliShop_PHP/Empresa/edit_profile", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
