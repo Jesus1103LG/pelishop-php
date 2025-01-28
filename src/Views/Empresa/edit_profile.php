aff47f53dd278b94437ff92c50023b3fb8fcf014
Jesus1103LG <rieteocaeras@gmail.com>
2025-01-28 06:49:32 -0400

    feat: add product detail view and update/delete functionality

---
+++ src/Views/Empresa/edit_profile.php
@@ -0,0 +1,112 @@
+<?php $subTitulo = "Edit Profile"; ?>
+<?php include("src/layout/header.php"); ?>
+<?php
+$nom = explode(" ", $persona["nombre"]);
+
+if (count($nom) > 2) {
+    $nom = [$nom[0] . " " . $nom[1], $nom[2] . " " . $nom[3]];
+}
+
+if ($persona["direccion_id"] != 1) {
+    $direccion_persona = get_direccion($persona["direccion_id"]);
+    $ciudad_persona = get_ciudad($direccion_persona["ciudades_id"]);
+    $estado_persona = $ciudad_persona["estados_id"];
+}
+?>
+
+<div class="content">
+    <form id="form" method="POST" action="" style="width: 50%;" enctype="multipart/form-data">
+        <div class="form-div">
+            <label for="tipo-doc cedula">Documento de identidad</label>
+            <div class="identidad">
+                <select name="tipo-doc" id="tipo-doc" required>
+                    <option value="" disabled selected>Seleccione una opción</option>
+                    <?php
+                    $tipos = ["V", "E", "J", "G"];
+                    foreach ($tipos as $tipo) :
+                    ?>
+                        <option value="<?= $tipo ?>" <?= $persona["identidad"] == $tipo ? "selected" : "" ?>>
+                            <?= $tipo ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+                <input name="cedula" id="cedula" type="text" value="<?= $persona["cedula"]; ?>" required />
+            </div>
+        </div>
+        <div class="form-div">
+            <label for="nombre">Nombre</label>
+            <input name="nombre" id="nombre" type="text" value="<?= $nom[0]; ?>" required />
+        </div>
+        <div class="form-div">
+            <label for="apellido">Apellido</label>
+            <input name="apellido" id="apellido" type="text" value="<?= $nom[1]; ?>" required />
+        </div>
+        <div class="form-div">
+            <label for="email">Email</label>
+            <input name="email" id="email" type="email" value="<?= $persona["email"]; ?>" required />
+        </div>
+        <div class="form-div">
+            <label for="telefono">Telefono</label>
+            <div id="form-div-tlf">
+                <input class="code" type="text" required readonly value="+58" />
+                <input
+                    name="telefono"
+                    id="telefono"
+                    type="number"
+                    required
+                    max="9999999999"
+                    oninput="limitDigits(this)"
+                    value="<?= $persona["telefono"]; ?>" />
+            </div>
+        </div>
+        <div class="form-div">
+            <label for="fechaNc">Fecha de nacimiento</label>
+            <input name="fechaNc" id="fechaNc" type="date" min="1960-01-01" max="<?= $fecha ?>" value="<?= $persona["fecha_nc"] ?>" required />
+        </div>
+        <div class="form-div">
+            <label for="direccion">Direccion</label>
+            <?php if ($persona["direccion_id"] == 1): ?>
+                <div id="form-div-tlf">
+                    <input style="width: 33%;" placeholder="Calle, Av, Barrio, etc." name="direccion" id="direccion" type="text" required />
+                    <select style="width: 33%;" name="estado" id="estado" required style="width: 100%;" onchange="cargarCiudadesEmpresa(this.value)">
+                        <option value="" disabled selected>Seleccione tu estado</option>
+                        <?php foreach ($estados as $estado): ?>
+                            <option value="<?= $estado["id"] ?>"><?= $estado["estado"] ?></option>
+                        <?php endforeach ?>
+                    </select>
+                    <select style="width: 33%;" name="ciudad" id="ciudad" required style="width: 100%;">
+                        <option value="" disabled selected>Seleccione tu ciudad</option>
+                    </select>
+                </div>
+            <?php else: ?>
+                <div id="form-div-tlf">
+
+                    <input style="width: 33%;" name="direccion" id="direccion" type="text" value="<?= $direccion_persona["calle"] ?>" required />
+                    <select style="width: 33%;" name="estado" id="estado" required style="width: 100%;" onchange="cargarCiudadesEmpresa(this.value)">
+                        <option value="" disabled selected>Seleccione tu estado</option>
+                        <?php foreach ($estados as $estado): ?>
+                            <option value="<?= $estado["id"] ?>" <?= $estado["id"] == $estado_persona ? "selected" : "" ?>><?= $estado["estado"] ?></option>
+                        <?php endforeach ?>
+                    </select>
+                    <select style="width: 33%;" name="ciudad" id="ciudad" required style="width: 100%;">
+                        <option value="" disabled selected>Seleccione tu ciudad</option>
+                        <option value="<?= $ciudad_persona["id"] ?>" selected><?= $ciudad_persona["ciudad"] ?></option>
+                    </select>
+                </div>
+            <?php endif; ?>
+        </div>
+        <div class="form-div">
+            <label for="image">Foto de Perfil</label>
+            <input name="image" id="image" type="file" value="<?= $persona["foto_perfil"] ?>" required />
+        </div>
+        <div style="display: flex; gap: 10px;">
+            <button style="width: 100%;" id="btn_edit" name="accion" value="update">EDIT</button>
+        </div>
+    </form>
+</div>
+<script src="/peliShop_PHP/src/public/javascript/signup.js"></script>
+<script src="/peliShop_PHP/src/public/javascript/edit_profile.js"></script>
+
+</body>
+
+</html>
--- src/controllers/EmpresaController.php
+++ src/controllers/EmpresaController.php
@@ -1,7 +1,11 @@
 <?php
 require_once("src/helpers/auth.php");
+require_once("src/helpers/loadImage.php");
 require_once("src/model/personaDB.php");
 require_once("src/model/productoDB.php");
+require_once("src/model/estadosDB.php");
+require_once("src/model/ciudadesDB.php");
+require_once("src/model/direccionDB.php");
 class EmpresaController
 {
     public function home()
@@ -50,6 +54,117 @@ class EmpresaController
         }
 
         include("src/Views/Empresa/productoCreate.php");
+    }
+
+    public function producto_detail($id)
+    {
+        requireAuth();
+        noRedirectToOtherRol();
+
+        $producto = get_producto($id);
+
+        try {
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $data = [
+                    "nombre" => $_POST["nombre"],
+                    "precio" => $_POST["precio"],
+                    "stock" => $_POST["stock"],
+                    "talla" => $_POST["talla"],
+                    "color" => $_POST["color"],
+                    "persona_cedula" => $_POST["distribuidor"]
+                ];
+
+                if (isset($_POST["accion"])) {
+                    switch ($_POST["accion"]) {
+                        case 'update':
+                            update_producto($id, $data);
+                            header("Location: ../productos");
+                            break;
+                        case 'delete':
+                            delete_producto($id);
+                            header("Location: ../productos");
+                            break;
+                        default:
+                            echo "Accion no reconocida.";
+                            break;
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            die("ERROR:" . $e->getMessage());
+        }
+        include("src/Views/Empresa/productDetail.php");
+    }
+
+    public function profile()
+    {
+        requireAuth();
+        noRedirectToOtherRol();
+
+        $datos = get_persona_email($_SESSION["email"]);
+
+        include("src/Views/Client/profile.php");
+    }
+
+    public function edit_profile()
+    {
+        requireAuth();
+        noRedirectToOtherRol();
+        $persona = get_persona_email($_SESSION["email"]);
+
+        $estados = get_all_estados();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // Leer los datos enviados en la solicitud POST
+            $input = json_decode(file_get_contents('php://input'), true);
+            var_dump($input);
+            // Validar que se haya enviado el estado_id
+            if (isset($input['estado_id'])) {
+                $estado_id = intval($input['estado_id']);
+                $ciudades = get_ciudades_by_estado($estado_id);
+
+                // Generar las opciones de ciudades como HTML
+                $html = '<option value="" disabled selected>Seleccione tu ciudad</option>';
+                foreach ($ciudades as $ciudad) {
+                    $html .= ("<option value='" . $ciudad["id"] . "'>" . $ciudad["ciudad"] . "</option>");
+                }
+
+                // Devolver el fragmento HTML
+                echo $html;
+                exit;
+            }
+
+            if (isset($_POST["accion"])) {
+                if ($persona["direccion_id"] == 1) {
+                    $direccion_nueva = create_direccion($_POST["direccion"], $_POST["estado"], $_POST["ciudad"]);
+                }
+
+                $profile_photo = $_FILES["image"]["name"] == $persona["foto_perfil"] ? $persona["foto_perfil"] : uploadImage("image");
+
+                $data = [
+                    "cedula" => $persona["cedula"],
+                    "identidad" => $persona["identidad"],
+                    "nombre" => $_POST["nombre"] . " " . $_POST["apellido"],
+                    "email" => $_POST["email"],
+                    "telefono" => $_POST["telefono"],
+                    "fecha_nc" => $_POST["fechaNc"],
+                    "direccion" => $direccion_nueva ? $direccion_nueva : $persona["direccion_id"],
+                    "foto_perfil" => $_FILES["image"]["name"] == "default.png" ? "default.png" : $profile_photo,
+                    "rol" => $persona["roles_id"]
+                ];
+
+                switch ($_POST["accion"]) {
+                    case 'update':
+                        update_persona($data);
+                        header("Location: /peliShop_PHP/Empresa/profile");
+                        break;
+                    default:
+                        echo "Accion no reconocida.";
+                        break;
+                }
+            }
+        }
+
+        include("src/Views/Empresa/edit_profile.php");
     }
 
     public function _404()
