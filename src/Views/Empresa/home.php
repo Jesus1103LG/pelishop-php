e805136cb9cdda1e1a64ab9376d79989ea98823c
Jesus1103LG <rieteocaeras@gmail.com>
2025-01-27 23:29:41 -0400

    feat: add product listing and creation functionality for Empresa

--- src/Views/Empresa/home.php
+++ src/Views/Empresa/home.php
@@ -1,6 +1,49 @@
 <?php $subTitulo = "Home"; ?>
 <?php include("src/layout/header.php"); ?>
 
+<main class="table-container" style="margin-top: 2rem;">
+    <div style="display: flex; justify-content: space-between;">
+        <h2>Listado de mis Productos</h2>
+        <a href="producto_create" class="action-link">Crear Productos</a>
+    </div>
+    <table class="custom-table">
+        <thead>
+            <tr>
+                <th>IMG</th>
+                <th>nombre</th>
+                <th>precio</th>
+                <th>stock</th>
+                <th>talla</th>
+                <th>color</th>
+                <th>distribuidor</th>
+                <th>Acción</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php if ($productos): ?>
+                <?php foreach ($productos as $producto): ?>
+                    <tr>
+                        <td><img style="width: 40px; height: 40px; object-fit: cover; border-radius: 100%;" src="/peliShop_PHP/src/uploads/<?= $producto["foto_producto"] ?>" alt=""></td>
+                        <td><?= $producto["nombre"]; ?></td>
+                        <td><?= $producto["precio"]; ?></td>
+                        <td><?= $producto["stock"]; ?></td>
+                        <td><?= $producto["talla"]; ?></td>
+                        <td><?= $producto["color"]; ?></td>
+                        <td><?= $persona["email"]; ?></td>
+                        <td>
+                            <a href="producto_detail/<?= $producto["id"] ?>" class="action-link">Editar</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="8">No tienes ningun productos</td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+</main>
+
 </body>
 
 </html>
--- src/controllers/EmpresaController.php
+++ src/controllers/EmpresaController.php
@@ -1,11 +1,55 @@
 <?php
+require_once("src/helpers/auth.php");
+require_once("src/model/personaDB.php");
+require_once("src/model/productoDB.php");
 class EmpresaController
 {
     public function home()
     {
         requireAuth();
         noRedirectToOtherRol();
-        include("src/Views/Admin/home.php");
+        $persona = get_persona_email($_SESSION["email"]);
+        $productos = get_all_productos_by_persona($persona["cedula"]);
+        include("src/Views/Empresa/home.php");
+    }
+
+    public function productos()
+    {
+        requireAuth();
+        noRedirectToOtherRol();
+        $persona = get_persona_email($_SESSION["email"]);
+        $productos = get_all_productos_by_persona($persona["cedula"]);
+        include("src/Views/Empresa/home.php");
+    }
+
+    public function producto_create()
+    {
+        requireAuth();
+        noRedirectToOtherRol();
+        require_once("src/helpers/loadImage.php");
+        $empresa = get_persona_email($_SESSION["email"]);
+
+        try {
+            if (!empty($_POST)) {
+                $data = [
+                    "nombre" => $_POST["nombre"],
+                    "precio" => $_POST["precio"],
+                    "stock" => $_POST["stock"],
+                    "talla" => $_POST["talla"],
+                    "color" => $_POST["color"],
+                    "persona_cedula" => $_POST["distribuidor"],
+                    "foto_producto" => uploadImage("image")
+                ];
+
+                create_producto($data);
+                header("Location: /peliShop_PHP/Empresa/productos");
+                exit;
+            }
+        } catch (Exception $e) {
+            die("ERROR: " . $e->getMessage());
+        }
+
+        include("src/Views/Empresa/productoCreate.php");
     }
 
     public function _404()
