a1786e32f0450afc7c2fb1616d32678cee627340
Jesus1103LG <rieteocaeras@gmail.com>
2025-01-28 06:49:19 -0400

    fix: remove dashboard link from Empresa navigation

--- src/layout/navEmpresa.php
+++ src/layout/navEmpresa.php
@@ -1,5 +1,4 @@
 <ul class="navbar-links">
     <li><a href="/peliShop_PHP/Empresa/home">HOME</a></li>
     <li><a href="/peliShop_PHP/Empresa/productos">PRODUCTOS</a></li>
-    <li><a href="/peliShop_PHP/Empresa/dashboard">DASHBOARD</a></li>
 </ul>
