352dd96bc61d5b49ade32f361c5903f8e32ec362
Jesus1103LG <rieteocaeras@gmail.com>
2025-01-28 22:15:31 -0400

    refactor: remove obsolete DOCS link from admin navigation

--- src/layout/navAdmin.php
+++ src/layout/navAdmin.php
@@ -1,5 +1,4 @@
 <ul class="navbar-links">
     <li><a href="/peliShop_PHP/Admin/home">HOME</a></li>
     <li><a href="/peliShop_PHP/Admin/tablas">TABLAS</a></li>
-    <li><a href="/peliShop_PHP/Admin/docs">DOCS</a></li>
 </ul>
